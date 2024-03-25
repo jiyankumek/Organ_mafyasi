3f626d16b5a395c4243f44984e17f76e04c57726
jiyan kumek <95100321+jiyankumek@users.noreply.github.com>
2024-03-25 23:40:57 +0300

    kesme islemi yapildi

--- Assets/Scripts/jiyan/PC.cs
+++ Assets/Scripts/jiyan/PC.cs
@@ -8,10 +8,19 @@ public class PC : MonoBehaviour
     public float raycastLength = 5f; // Raycast ýþýnýnýn uzunluðu
     public GameObject PcCanvas; // Açýlacak olan canvas
     public bool pcCanvasisTrue;
+    public bool releaseFlag = false;
     public GameObject sedye;
+    public GameObject kesilecekYer;
+    public GameObject kalp;
+    public GameObject heldObject; // Elinde tutulan objeyi tutmak için bir GameObject referansý
+
+
+    public Transform hand;
     public Vector3 SpawVector3;
 
     private Economy economy;
+
+
 
 
     void Start()
@@ -22,7 +31,7 @@ public class PC : MonoBehaviour
 
     void Update()
     {
-        if (pcCanvasisTrue == true)
+        if (pcCanvasisTrue)
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
@@ -32,30 +41,83 @@ public class PC : MonoBehaviour
         }
         else // Canvas kapalýyken
         {
-            RaycastHit hit;
-            // Karakterden ileri doðru bir raycast ýþýný çýkar
-            if (Physics.Raycast(transform.position, transform.forward, out hit, raycastLength))
+            Eline_Obje_Alip_Birakma();
+
+            if (Input.GetKeyDown(KeyCode.Escape))
             {
-                // Eðer raycast ýþýný "pc" etiketli bir nesneye çarparsa
-                if (hit.collider.CompareTag("pc"))
-                {
-                    Debug.Log("hit");
-                    // Eðer "E" tuþuna basýlýrsa
-                    if (Input.GetKeyDown(KeyCode.E))
-                    {
-                        // Canvas'ý aktif hale getir
-                        PcCanvas.gameObject.SetActive(true);
-                        pcCanvasisTrue = true;
-                    }
-                }
+                PcCanvas.gameObject.SetActive(false);
+                pcCanvasisTrue = false;
             }
         }
-        if (Input.GetKeyDown(KeyCode.Escape))
+    }
+
+    private void Eline_Obje_Alip_Birakma()
+    {
+        RaycastHit hit;
+        // Karakterden ileri doðru bir raycast ýþýný çýkar
+        if (Physics.Raycast(transform.position, transform.forward, out hit, raycastLength))
         {
-            PcCanvas.gameObject.SetActive(false);
-            pcCanvasisTrue = false;
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (hit.collider.CompareTag("pc"))
+                {
+                    PcCanvas.SetActive(true);
+                    pcCanvasisTrue = true;
+                }
+
+                if (hit.collider.CompareTag("obje") &&
+                    heldObject == null) // Eðer 'obje'ye basýldýysa ve elinde bir obje yoksa
+                {
+                    Debug.Log("nester ");
+                    heldObject = hit.collider.gameObject;
+                    heldObject.transform.SetParent(hand);
+                    heldObject.transform.localPosition = Vector3.zero;
+                    heldObject.GetComponent<Rigidbody>().isKinematic = true;
+                    heldObject.GetComponent<Rigidbody>().useGravity = false;
+                    heldObject.GetComponent<Collider>().enabled = false;
+                }
+
+                if (hit.collider.CompareTag("kalp") && heldObject == null) // Eðer 'obje'ye basýldýysa ve elinde bir obje yoksa
+                {
+                    Debug.Log("nester ");
+                    heldObject = hit.collider.gameObject;
+                    heldObject.transform.SetParent(hand);
+                    heldObject.transform.localPosition = Vector3.zero;
+                    heldObject.GetComponent<Rigidbody>().isKinematic = true;
+                    heldObject.GetComponent<Rigidbody>().useGravity = false;
+                    heldObject.GetComponent<Collider>().enabled = false;
+                }
+            }
+
+            if (hit.collider.CompareTag("kesilecek"))
+            {
+                Debug.Log("KES");
+                if (heldObject != null && Input.GetMouseButton(0))
+                {
+                    kesilecekYer.SetActive(false);
+                    kalp.GetComponent<Rigidbody>().isKinematic = false;
+                    kalp.GetComponent<Rigidbody>().useGravity = true;
+                    
+                }
+                
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.F) && heldObject != null) // Eðer 'E' tuþu býrakýldýysa ve elinde bir obje varsa 
+        {
+            Debug.Log("birak");
+            heldObject.GetComponent<Rigidbody>().isKinematic = false;
+            heldObject.GetComponent<Rigidbody>().useGravity = true;
+            heldObject.GetComponent<Collider>().enabled = true;
+            heldObject.transform.SetParent(null);
+            heldObject = null;
         }
     }
+
+
+
+
+
 
 
     public void Buy_Sedye()
