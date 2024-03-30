b0f4f7830528466380ba177b8f7077384f1718db
jiyan kumek <95100321+jiyankumek@users.noreply.github.com>
2024-03-30 23:41:57 +0300

    Revert "."
    This reverts commit 502eefec9f031200c5d2c730d0c6fcfdc5082b3b.

--- Assets/Scripts/kadir/dayd.cs
+++ Assets/Scripts/kadir/dayd.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class Dayd : MonoBehaviour
+public class dayd : MonoBehaviour
 {
     public float dayDuration = 86400f; // Günün saniye cinsinden süresi (24 saat x 60 dakika x 60 saniye)
     public Light sun; // Güneþ ýþýðý
@@ -42,15 +42,15 @@ public class Dayd : MonoBehaviour
     void UpdateLighting()
     {
         float t = timeOfDay / (24f * 3600f);
-        //sun.transform.localRotation = Quaternion.Euler(t * 360f - 90, 170, 0); // Güneþin dönmesi
+        sun.transform.localRotation = Quaternion.Euler(t * 360f - 90, 170, 0); // Güneþin dönmesi
 
         // Arka plan rengini güncelle (gündüzden geceye geçerken geçiþ yap)
         RenderSettings.ambientLight = Color.Lerp(dayColor, nightColor, t * t);
 
         // Hava yavaþ yavaþ kararsýn, örneðin 19:00'dan sonra
-        if (timeOfDay >= 19f * 3600f)
+        if (timeOfDay >= 1f * 3600f)
         {
-            sun.intensity = Mathf.Lerp(19f, 0.1f, (timeOfDay - 1f * 3600f) / (5f * 3600f)); // 19:00'dan sonra yavaþ yavaþ karanlýklaþma
+            sun.intensity = Mathf.Lerp(1f, 0.1f, (timeOfDay - 1f * 3600f) / (5f * 3600f)); // 19:00'dan sonra yavaþ yavaþ karanlýklaþma
         }
         else
         {
