f0f9b6672bfb5b762ee1e9bb3c96b5889ca877b3
kadir1356 <110829810+kadir1356@users.noreply.github.com>
2024-04-01 21:40:22 +0300

    1

--- Assets/Scripts/kadir/health2.cs
+++ Assets/Scripts/kadir/health2.cs
@@ -7,7 +7,7 @@ using UnityEditor;
 
 public class health2 : MonoBehaviour
 {
-    public Image healthBar;
+    public Image healthBarImage; // Canbarýnýzý buraya sürükleyin
     public TMP_Text healthText;
     public Image gameOverImage; // Oyun sonu ekranýnýzý buraya sürükleyin
     private int health = 100;
@@ -40,8 +40,12 @@ public class health2 : MonoBehaviour
 
     void UpdateHealthBar()
     {
-        healthBar.fillAmount = (float)health / 100;
+        float healthPercentage = (float)health / 100;
+        healthBarImage.fillAmount = healthPercentage;
         healthText.text = "Can: " + health.ToString();
+
+        // Can deðeri azaldýkça Image bileþeninin boyutunu azalt
+        healthBarImage.rectTransform.sizeDelta = new Vector2(healthPercentage * 100, healthBarImage.rectTransform.sizeDelta.y);
     }
 
     void GameOver()
