72aee23a5e46179b867b9ed5c564db6c6c076b4c
jiyan kumek <95100321+jiyankumek@users.noreply.github.com>
2024-04-22 23:14:48 +0300

    can barı npcye eklendi

--- Assets/Scripts/kadir/can1.cs
+++ Assets/Scripts/kadir/can1.cs
@@ -8,22 +8,20 @@ public class can1 : MonoBehaviour
 {
     public Image healthBar;
     public TMP_Text healthText;
-    public Image gameOverImage; // Oyun sonu image'ý
+   // public Image gameOverImage; // Oyun sonu image'ý
     private float maxHealth = 100f;
     private float currentHealth;
 
     void Start()
     {
         currentHealth = maxHealth;
-        gameOverImage.enabled = false; // Oyun baþladýðýnda image'ý gizle
+        
+        TakeDamage(Random.Range(10,50));
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && currentHealth > 0)
-        {
-            TakeDamage(10);
-        }
+        
     }
 
     void TakeDamage(float damage)
@@ -35,7 +33,7 @@ public class can1 : MonoBehaviour
         if (currentHealth <= 0)
         {
             // Can deðeri 0'a düþtüðünde image'ý göster ve can texti ile can barýný gizle
-            gameOverImage.enabled = true;
+            
             healthBar.enabled = false;
             healthText.enabled = false;
         }
